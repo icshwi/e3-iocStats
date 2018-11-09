f0c2f5c29e6192b0a7f828086969d81469ff040c
Jeong Han Lee <jeonghan.lee@gmail.com>
2018-11-09 15:19:05 +0100

    update iocStats.cmd

--- cmds/iocStats.cmd
+++ cmds/iocStats.cmd
@@ -1,6 +1,17 @@
+require iocStats,ae5d083
+
+epicsEnvSet("SEC", "SEC")
+epicsEnvSet("SUB", "SUB01")
+epicsEnvSet("P", "$(SEC)-$(SUB):")
+epicsEnvSet("DIS", "DIS")
+epicsEnvSet("DEV", "DEV-01")
+epicsEnvSet("R", "$(DIS)-$(DEV)")
+epicsEnvSet("IOCNAME", "$(P)$(R)")
 
 
-#
-## IocStats
-#- iocshLoad "$(TOP)/iocStats.cmd" "IOC=$(IOC):IocStats"
-dbLoadRecords("iocAdminSoft.db", "IOC=$(IOC)")
+loadIocsh("iocStats.iocsh", "IOCNAME=$(IOCNAME)")
+
+iocInit()
+
+dbl > "${IOCNAME}_PVs.list"
+
